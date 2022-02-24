56f40315cba3874d9394cb43bf1d4152703455ab
KoaHodoK <koahodok@gmail.com>
2022-02-24 09:35:54 +0630

    final touch

--- lib/home_calculate.dart
+++ lib/home_calculate.dart
@@ -1,5 +1,5 @@
+import 'dart:math';
 import 'package:bmi_calculator/enum_file.dart';
-import 'package:bmi_calculator/result.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -16,6 +16,68 @@ class _CalculatePageState extends State<CalculatePage> {
   double slidervalue = 180.0;
   double weight = 50;
   int age = 50;
+
+  void calculate(double height, double weight, BuildContext context) {
+    double res = weight / pow(height / 100, 2);
+    String msg = "";
+    Icon iconData = const Icon(Icons.sentiment_neutral);
+
+    if (res <= 18.5) {
+      msg = "Underweight";
+      iconData = const Icon(
+        Icons.sentiment_dissatisfied,
+        color: Colors.amber,
+        size: 50,
+      );
+    } else if (res > 18.5 && res <= 25) {
+      msg = "Normal";
+      iconData = const Icon(
+        Icons.sentiment_very_satisfied,
+        color: Colors.amber,
+        size: 50,
+      );
+    } else if (res > 25 && res <= 30) {
+      msg = "Overweight";
+      iconData = const Icon(
+        Icons.sentiment_dissatisfied,
+        color: Colors.amber,
+        size: 50,
+      );
+    } else {
+      msg = "Obese";
+      iconData = const Icon(
+        Icons.sentiment_very_dissatisfied,
+        color: Colors.amber,
+        size: 50,
+      );
+    }
+
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: iconData,
+            content: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("You BMI ${res.toStringAsFixed(1)}"),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text("You are $msg"),
+                ]),
+            actions: [
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("OK"))
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -260,13 +322,14 @@ class _CalculatePageState extends State<CalculatePage> {
                 padding: const EdgeInsets.all(8.0),
                 child: InkWell(
                   onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (_) => ResultBMI(
-                                height: slidervalue,
-                                weight: weight,
-                                age: age)));
+                    calculate(slidervalue, weight, context);
+                    // Navigator.push(
+                    //     context,
+                    //     MaterialPageRoute(
+                    //         builder: (_) => ResultBMI(
+                    //             height: slidervalue,
+                    //             weight: weight,
+                    //             age: age)));
                   },
                   child: Container(
                       alignment: Alignment.center,
